d09ce715a9c479ad33fdfdb5b21421e43ab67994
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-03-13 02:08:15 +0100

    Initial commit

---
+++ example-arrow-function.js
@@ -0,0 +1,48 @@
+var names = ['Vegeta', 'Son Goku', 'Trunks'];
+//
+// names.forEach(function (name) {
+//   console.log('forEach', name);
+// });
+//
+
+// //Arrow Functions
+// names.forEach((name) => {
+//   console.log('arrowFunc', name);
+// });
+//
+// names.forEach((name) => console.log(name));
+// var returnMe = (name) => name + '!';
+// console.log(returnMe('Andrew'));
+
+// var person = {
+//   name: 'Shan',
+//   greet: function (){
+//     names.forEach((name) => {
+//       console.log(this.name + ' says hi to ' + name)
+//     });
+//   }
+// };
+//
+// person.greet();
+
+
+//Challenge Area
+function add(a, b) {
+  return a + b;
+}
+
+
+//addStatement
+var addStatement = (a, b) => {
+  return a + b;
+}
+console.log(addStatement(4, 7));
+
+//addExpression
+
+var addExpression = (a, b) => a + b;
+console.log(addExpression(4, -4));
+
+
+// console.log(add(1, 3));
+// console.log(add(9, 0));
